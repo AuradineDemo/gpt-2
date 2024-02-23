4c2e73de391a6a763be51445f5e61410c4f08ce7
sdevaraju1u <sdevarajulu@auradine.com>
2024-02-23 10:13:04 -0800

    adding a function for traces

--- src/test.py
+++ src/test.py
@@ -4,5 +4,18 @@ def main():
     for i in range(10):
         print(i)
 
+        def fibonacci_sum(start, end):
+            fib_sum = 0
+            fib_prev = 0
+            fib_curr = 1
+
+            while fib_curr <= end:
+                if fib_curr >= start:
+                    fib_sum += fib_curr
+                fib_prev, fib_curr = fib_curr, fib_prev + fib_curr
+
+            return fib_sum
+
+
 if __name__ == '__main__':
     main()
